9d53bb37b10d46b9c6fd5f96c17421dd9d771969
kayalkamli <Kayalvizhi.Karikalan@dexian.com>
2025-06-09 17:27:48 +0530

    insert category data

--- myapp/blog/models.py
+++ myapp/blog/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 
+#Category
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self) :
+        return self.name 
+    
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     img_url = models.URLField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
     
     def __str__(self) :
         return self.title
+    
